9b8f3c2e167330fb9319550f6db70e3ccda4c577
LastWeekNextDay <doesn1999@gmail.com>
2023-03-03 10:58:35 +0200

    First Commit

---
+++ RSA.cs
@@ -0,0 +1,7 @@
+﻿namespace RSAAlgorithm
+{
+    public class Rsa
+    {
+        
+    }
+}
---
+++ RsaUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace RSAAlgorithm
+{
+    public static class RsaUtil
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+
+            if (n % 2 == 0)
+            {
+                return n == 2;
+            }
+
+            var l = (long)(Math.Sqrt(n) + 0.5);
+
+            for (var i = 3; i <= l; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
